90fa5a7b732ab49b3a0ab5f2f90588ccda7746ed
Striver <harshjain2720@gmail.com>
2023-04-05 19:51:38 +0530

    Added profile page skeleton

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -18,6 +18,34 @@ class AuthMethods {
     return model.User.fromSnap(documentSnapshot);
   }
 
+// update user details
+// add default parameter values to avoid errors
+  Future<String> updateUserDetails(
+      Uint8List? img, String? username, String? contact) async {
+    User currentUser = _auth.currentUser!;
+    String res = "Some error Occurred";
+    try {
+      String photoUrl = await StorageMethods()
+          .uploadImageToStorage('profilePics', img!, false, false);
+      model.User _user = model.User(
+        username: username!,
+        uid: currentUser.uid,
+        photoUrl: photoUrl,
+        email: currentUser.email!,
+        contact: contact!,
+      );
+      await _firestore
+          .collection("users")
+          .doc(currentUser.uid)
+          .set(_user.toJson());
+      //return _user;
+      res = "success";
+    } catch (e) {
+      return e.toString();
+    }
+    return res;
+  }
+
   // Signing Up User
 
   Future<String> signUpUser({
